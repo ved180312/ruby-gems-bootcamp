bf95de24a1b43f893cc3cfe21063f61250e5c32a
Ved Tiwari <ved@webkorps.com>
2022-11-23 00:00:46 +0530

    Add static pages controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  root 'static_pages#landing_pages'
+  # get 'static_pages/landing_pages'
+  get 'static_pages/privacy_policy'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
